1e9c779cee6b1216379ade99f85a109bd2f1ccc8
Om Dwivedi <dwivediomacc2@gmail.com>
2023-02-25 20:09:27 +0530

    commit

--- lib/Pages/settingPage.dart
+++ lib/Pages/settingPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingPage extends StatefulWidget {
   SettingPage({Key? key}) : super(key: key);
@@ -11,8 +13,51 @@ class _SettingPageState extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Container(
-      child: Text("Setting"),
-    ));
+        child:Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: "Search"
+              ),
+            ),
+            SettingsList(
+            sections: [
+            SettingsSection(
+              title: Text('Common'),
+              tiles: <SettingsTile>[
+                SettingsTile.navigation(
+                  leading: Icon(Icons.language),
+                  title: Text('Language'),
+                  value: Text('English'),
+                ),
+                SettingsTile.switchTile(
+                  onToggle: (value) {},
+                  initialValue: true,
+                  leading: Icon(Icons.format_paint),
+                  title: Text('Enable custom theme'),
+                ),
+              ],
+
+            ),
+            SettingsSection(
+              title: Text('Account'),
+              tiles: <SettingsTile>[
+                SettingsTile.navigation(
+                  leading: Icon(Icons.phone),
+                  title: Text('Phone Number'),
+                ),
+                SettingsTile.navigation(
+                  leading: Icon(Icons.email),
+                  title: Text('Email'),
+                ),
+              ],
+              
+            ),
+      ],
+    ),
+          ],
+        ), 
+    );
   }
 }
