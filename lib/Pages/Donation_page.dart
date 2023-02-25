a91387e826a55faf4d2de861065dd64962130a82
Om Dwivedi <dwivediomacc2@gmail.com>
2023-02-25 16:00:54 +0530

    my commit

--- lib/Pages/Donation_page.dart
+++ lib/Pages/Donation_page.dart
@@ -60,8 +60,8 @@ class _DonatePetState extends State<DonatePet> {
               // )
               )
           .then((value) {
-            log("Done")
-          });
+        log("Done");
+      });
 
       if (FinalImage != null) {
         setState(() {
